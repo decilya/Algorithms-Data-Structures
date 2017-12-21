2aa864351607b1b33484585be6d46ba46ee19045
via <via@wizardforum.ru>
2017-12-21 13:56:14 +0300

    lesson_3

--- lesson_3/src/ilya_v/java/algorithms/lesson_3/Main.java
+++ lesson_3/src/ilya_v/java/algorithms/lesson_3/Main.java
@@ -6,12 +6,14 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static int[] bubbleSort(int[] arr){
+    private static int countBubbleSort(int[] arr) {
 
-        int tmp, count = 0;
-        for (int i = 0; i < arr.length; i++){
-            for (int j =0; j < arr.length - 1; j++){
-                if (arr[j] > arr[j + 1]){
+        int tmp;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
@@ -20,8 +22,33 @@ public class Main {
             }
         }
 
-        return arr;
+        return count;
     }
+
+
+    private static int fixCountBubbleSort(int[] arr) {
+
+        int tmp;
+        int count = 0;
+        boolean f;
+
+        for (int i = 0; i < arr.length; i++) {
+            f = false;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1] && !f) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    count++;
+                    f = true;
+                }
+            }
+
+        }
+
+        return count;
+    }
+
 
     public static void main(String[] args) {
 
@@ -30,17 +57,17 @@ public class Main {
             Сравнить количество операций сравнения оптимизированной и не оптимизированной программы.
             Написать функции сортировки, которые возвращают количество операций.
         */
-        int[] arr = {1, 2, 33, 6, 8, 10, 23, 5, 7};
+        int[] arr = {8, 5, 4, 3, 2, 1, 10, 11, 12, 23, 33, 33, 81, 80, 82, 83, 99};
+        System.out.println("count = " + countBubbleSort(arr) + "; arr = " + Arrays.toString(arr));
 
-        System.out.println(Arrays.toString(bubbleSort(arr)));
-
+        int[] newArr = {8, 5, 4, 3, 2, 1, 10, 11, 12, 23, 33, 33, 81, 80, 82, 83, 99};
+        System.out.println("count = " + fixCountBubbleSort(newArr) + "; arr = " + Arrays.toString(newArr));
 
 
        /*
         3.  Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив.
             Функция возвращает индекс найденного элемента или -1, если элемент не найден.
         */
-
 
     }
 }
