b3f16c6afa862412aa7527743cf48786f80714e4
decilya <decilya@gmail.com>
2017-12-24 10:15:29 +0300

    start lesson_4

---
+++ lesson_4/src/ilya_v/java/algorithms/lesson_4/Main.java
@@ -0,0 +1,11 @@
+package ilya_v.java.algorithms.lesson_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+
+
+
+    }
+}
