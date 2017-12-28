0c32155e9149afc3d3236d7bb1fcf3b291559be5
via <via@wizardforum.ru>
2017-12-28 14:21:56 +0300

    start lesson_5

---
+++ lesson_5/src/ilya_v/java/algorithms/lesson_5/Main.java
@@ -0,0 +1,8 @@
+package ilya_v.java.algorithms.lesson_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Домашняя работа урока номер 5 будет тут в течении новогодних праздников
+    }
+}
